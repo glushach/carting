2ef6de1b7ac3a0078707a5ef56d489be09245e2c
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-01 21:26:32 +0300

    feat: done card section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-
+new Swiper('.image-slider', {
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  },
+  // autoHeight: true,
+  spaceBetween: 40,
+  slidesPerGroup: 3,
+  // centeredSlides: true
+  loop: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1
+    },
+    1500: {
+      slidesPerView: 3,
+    },
+  }
+});
