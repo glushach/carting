96a92eba6313a50f8768b0169dc009732332c772
Oleksandr Hlushach <shoppingplaneta@gmail.com>
2022-08-03 10:34:49 +0300

    fix: style for slick slider

--- js/main.js
+++ js/main.js
@@ -4,14 +4,15 @@ new Swiper('.image-slider', {
     onlyInViewport: true,
     pageUpDown: true,
   },
-  // autoHeight: true,
   spaceBetween: 10,
   slidesPerGroup: 3,
-  // centeredSlides: true,
   loop: true,
   breakpoints: {
     320: {
       slidesPerView: 1
+    },
+    875: {
+      slidesPerView: 2,
     },
     1300: {
       slidesPerView: 3,
